ccb3635339666619d8634016db53b71bc3eae602
fafaisland <fafaisland@gmail.com>
2013-10-26 18:34:35 -0700

    New icons "T" iOS 7 updates

--- TimingMate_v2/TimingMate_v2/TMGlobal.h
+++
@@ -1,9 +0,0 @@
-//
-//  TMGlobal.h
-//  TimingMate_v2
-//
-//  Created by easonfafa on 6/29/13.
-//  Copyright (c) 2013 fafa. All rights reserved.
-//
-
-NSString * TMTimerStringFromSeconds(int seconds);
